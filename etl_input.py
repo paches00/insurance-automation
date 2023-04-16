16345f4ba0f62e04cb7367a6cb508807b59e4146
Juan Rubiales <61597804+jrubialesv@users.noreply.github.com>
2023-04-16 17:36:28 +0200

    Update of code with models output

--- etl_input.py
+++ etl_input.py
@@ -1,7 +1,7 @@
-def etl_format_input(data):
+def elt_format_input(data):
     # Transform the data to the correct format
     data["input"] = "input"
-    data = data.dropna()
+    # data = data.dropna()
     data = data.pivot(index='input',columns='campo', values='text').reset_index()
     data = data.drop(columns=['input'])
     
@@ -11,7 +11,53 @@ def etl_merge(hand, checkbox):
     return hand.merge(checkbox, left_index=True, right_index=True)
 
 def etl_input_checkbox(data):
-    
+    new_names  = ["Victima(s) incluso leve(s) — No",
+                    "Victima(s) incluso leve(s) — Si",
+                    "Daños materiales: Vehículos distintos de A y B — No",
+                    "Daños materiales: Vehículos distintos de A y B — Si",
+                    "Daños materiales: objetos distintos al vehículo — No",
+                    "Daños materiales: objetos distintos al vehículo — Si",
+                    "A Estaba estacionado/parado",
+                    "B Estaba estacionado/parado",
+                    "A Salía de un estacionamiento abriendo puerta",
+                    "B Salía de un estacionamiento abriendo puerta",
+                    "A Iba a estacionar",
+                    "B Iba a estacionar",
+                    "A Salia de un aparcamiento, de un Vehículo lugar privado, de un camino de tierra",
+                    "B Salia de un aparcamiento, de un Vehículo lugar privado, de un camino de tierra",
+                    "A Entrada a un aparcamiento, a un lugar privado, a un camino de tierra",
+                    "B Entrada a un aparcamiento, a un lugar privado, a un camino de tierra",
+                    "A Entrada a una plaza de sentido giratorio",
+                    "B Entrada a una plaza de sentido giratorio",
+                    "A Circulaba por una plaza de sentido giratono",
+                    "B Circulaba por una plaza de sentido giratono",
+                    "A Colisiono en la parte de atrás al otro vehiculo que circulaba en el mismo sentido y en el mismo carril",
+                    "B Colisiono en la parte de atrás al otro vehiculo que circulaba en el mismo sentido y en el mismo carril",
+                    "A Circulaba en el mismo sentido y en carril diferente",
+                    "B Circulaba en el mismo sentido y en carril diferente",
+                    "A Cambiaba de carril",
+                    "B Cambiaba de carril",
+                    "A Adelantaba",
+                    "B Adelantaba",
+                    "A Giraba a la derecha",
+                    "B Giraba a la derecha",
+                    "A Giraba a la izquierda",
+                    "B Giraba a la izquierda",
+                    "A Daba marcha atrás",
+                    "B Daba marcha atrás",
+                    "A Invadía la parte reservada a la circulación en sentido inverso",
+                    "B Invadía la parte reservada a la circulación en sentido inverso",
+                    "Vehiculo A — ¿Los daños propios del vehículo están asegurados? — No",
+                    "Vehiculo A — ¿Los daños propios del vehículo están asegurados? — Si",
+                    "Vehiculo B — ¿Los daños propios del vehículo están asegurados? — No",
+                    "Vehiculo B — ¿Los daños propios del vehículo están asegurados? — Si",
+                    "A Venía de la derecha (en un cruce)",
+                    "B Venía de la derecha (en un cruce)",
+                    "A No respeto la señal de preferencia o sematoro en rojo",
+                    "B No respeto la señal de preferencia o sematoro en rojo"
+                    ]
+    data.columns = new_names
+   
     # Si la columna 'victimas si' tiene x o X en la celda, se cambia por 1
     data['Vehiculo A — ¿Los daños propios del vehículo están asegurados?'] = data['Vehiculo A — ¿Los daños propios del vehículo están asegurados? — Si'].apply(lambda x: True if x == 'x' or x == 'X' else False)
     data = data.drop(columns=['Vehiculo A — ¿Los daños propios del vehículo están asegurados? — Si', 'Vehiculo A — ¿Los daños propios del vehículo están asegurados? — No'])
@@ -67,23 +113,22 @@ def etl_input_checkbox(data):
 
     return data
 
-def etl_original_data_format(hand_written, check):
+def etl_orginal_data_format(hand_written, check):
     hand_written = hand_written.rename(columns={'classes': 'campo'})
     hand_written = hand_written.rename(columns={'texto': 'text'})
     
     # append checkbox data
-    final = hand_written.concat(check)
-    # final = final.drop(columns=['Unnamed: 0'])
+    final = hand_written.append(check)
+    final = final.drop(columns=['Unnamed: 0'])
     return final
 
 def etl_main(hand_written, checkbox):
 
     hand_written = hand_written.rename(columns={'classes': 'campo'})
     hand_written = hand_written.rename(columns={'texto': 'text'})
-    hand_written = etl_format_input(hand_written)
+    hand_written = elt_format_input(hand_written)
 
-    checkbox1 = etl_input_checkbox(checkbox)
-    checkbox2 = etl_format_input(checkbox1)
-    input_file = etl_merge(hand_written, checkbox2)
+    checkbox = elt_format_input(checkbox)
+    input_file = etl_merge(hand_written, checkbox)
 
     return input_file

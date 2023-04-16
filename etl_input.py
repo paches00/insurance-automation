37278f3dca8da02ea2b67e377ed5f756104cfd62
Juan Rubiales <61597804+jrubialesv@users.noreply.github.com>
2023-04-16 13:05:10 +0200

    Update etl_input.py

--- etl_input.py
+++ etl_input.py
@@ -1,18 +1,76 @@
-def etl_format_input(data):
+def elt_format_input(data):
     # Transform the data to the correct format
     data["input"] = "input"
     data = data.dropna()
     data = data.pivot(index='input',columns='campo', values='text').reset_index()
     data = data.drop(columns=['input'])
-
-    return data
-
-def etl_merge(handwritten_data, checkbox_data):
-    # add checkbox data to handwritten data, merge on index
-    data = handwritten_data.merge(checkbox_data, left_index=True, right_index=True)
-
-    return data
     
+    new_col = ['victimas no', 'victimas si' , 'danos_materiales vehículos no', 'danos_materiales vehiculos si', 
+    'danos_materiales objetos no', 'danos_materiales objetos si', 
+    'A 1','B 1', 'A 2', 'B 2','A 3','B 3', 'A 4','B 4', 'A 5','B 5', 'A 6','B 6','A 7', 'B 7','A 8', 'B 8',
+    'A 9','B 9', 'A 10','B 10', 'A 11', 'B 11','A 12', 'B 12','A 13', 'B 13','A 14','B 14', 'A 15', 'B 15', 
+    'A  aseguradora danos_propios_no', 'A  aseguradora danos_propios_si',
+    'B aseguradora danos_propios no', 'B aseguradora danos_propios si',
+    'A 16','B 16', 'A 17','B 17']
+
+    # All columns that are not in new_col we use for hand
+    hand_df = data[data.columns.difference(new_col)]
+    checkbox = data[new_col]
+
+    checkbox = checkbox[new_col]
+
+    # change the name of the columns
+    new_names  = ["Victima(s) incluso leve(s) — No",
+                "Victima(s) incluso leve(s) — Si",
+                "Daños materiales: Vehículos distintos de A y B — No",
+                "Daños materiales: Vehículos distintos de A y B — Si",
+                "Daños materiales: objetos distintos al vehículo — No",
+                "Daños materiales: objetos distintos al vehículo — Si",
+                "A Estaba estacionado/parado",
+                "B Estaba estacionado/parado",
+                "A Salía de un estacionamiento abriendo puerta",
+                "B Salía de un estacionamiento abriendo puerta",
+                "A Iba a estacionar",
+                "B Iba a estacionar",
+                "A Salia de un aparcamiento, de un Vehículo lugar privado, de un camino de tierra",
+                "B Salia de un aparcamiento, de un Vehículo lugar privado, de un camino de tierra",
+                "A Entrada a un aparcamiento, a un lugar privado, a un camino de tierra",
+                "B Entrada a un aparcamiento, a un lugar privado, a un camino de tierra",
+                "A Entrada a una plaza de sentido giratorio",
+                "B Entrada a una plaza de sentido giratorio",
+                "A Circulaba por una plaza de sentido giratono",
+                "B Circulaba por una plaza de sentido giratono",
+                "A Colisiono en la parte de atrás al otro vehiculo que circulaba en el mismo sentido y en el mismo carril",
+                "B Colisiono en la parte de atrás al otro vehiculo que circulaba en el mismo sentido y en el mismo carril",
+                "A Circulaba en el mismo sentido y en carril diferente",
+                "B Circulaba en el mismo sentido y en carril diferente",
+                "A Cambiaba de carril",
+                "B Cambiaba de carril",
+                "A Adelantaba",
+                "B Adelantaba",
+                "A Giraba a la derecha",
+                "B Giraba a la derecha",
+                "A Giraba a la izquierda",
+                "B Giraba a la izquierda",
+                "A Daba marcha atrás",
+                "B Daba marcha atrás",
+                "A Invadía la parte reservada a la circulación en sentido inverso",
+                "B Invadía la parte reservada a la circulación en sentido inverso",
+                "Vehiculo A — ¿Los daños propios del vehículo están asegurados? — No",
+                "Vehiculo A — ¿Los daños propios del vehículo están asegurados? — Si",
+                "Vehiculo B — ¿Los daños propios del vehículo están asegurados? — No",
+                "Vehiculo B — ¿Los daños propios del vehículo están asegurados? — Si",
+                "A Venía de la derecha (en un cruce)",
+                "B Venía de la derecha (en un cruce)",
+                "A No respeto la señal de preferencia o sematoro en rojo",
+                "B No respeto la señal de preferencia o sematoro en rojo"
+                ]
+
+    checkbox.columns = new_names
+
+    data = hand_df.merge(checkbox, left_index=True, right_index=True)
+
+    return data
 
 def etl_input_checkbox(data):
     
@@ -25,16 +83,16 @@ def etl_input_checkbox(data):
     data = data.drop(columns=['Vehiculo B — ¿Los daños propios del vehículo están asegurados? — Si', 'Vehiculo B — ¿Los daños propios del vehículo están asegurados? — No'])
 
     # danos_materiales objetos no	danos_materiales objetos si	
-    data['Daños materiales: Vehículos distintos de A y B'] = data['Daños materiales: Vehículos distintos de A y B — si'].apply(lambda x: True if x == 'x' or x == 'X' else False)
-    data = data.drop(columns=['Daños materiales: Vehículos distintos de A y B — si', 'Daños materiales: Vehículos distintos de A y B — No'])
+    data['Daños materiales: Vehículos distintos de A y B'] = data['Daños materiales: Vehículos distintos de A y B — Si'].apply(lambda x: True if x == 'x' or x == 'X' else False)
+    data = data.drop(columns=['Daños materiales: Vehículos distintos de A y B — Si', 'Daños materiales: Vehículos distintos de A y B — No'])
 
     # danos_materiales vehiculos si	danos_materiales vehículos no
     data['Daños materiales: objetos distintos al vehículo'] = data['Daños materiales: objetos distintos al vehículo — Si'].apply(lambda x: True if x == 'x' or x == 'X' else False)
-    data = data.drop(columns=['Daños materiales: objetos distintos al vehículo — Si', 'Daños materiales:objetos distintos al vehículo — No'])
+    data = data.drop(columns=['Daños materiales: objetos distintos al vehículo — Si', 'Daños materiales: objetos distintos al vehículo — No'])
 
     # victimas no	victimas si
-    data['Victima(s) incluso leve(s)'] = data['Victima(s) incluso leve(s) — si'].apply(lambda x: True if x == 'x' or x == 'X' else False)
-    data = data.drop(columns=['Victima(s) incluso leve(s) — si', 'Victima(s) incluso leve(s) — no'])
+    data['Victima(s) incluso leve(s)'] = data['Victima(s) incluso leve(s) — Si'].apply(lambda x: True if x == 'x' or x == 'X' else False)
+    data = data.drop(columns=['Victima(s) incluso leve(s) — Si', 'Victima(s) incluso leve(s) — No'])
 
     # A 1	A 10	A 11	A 12	A 13	A 14	A 15	A 16	A 17	A 2	A 3	A 4	A 5	A 6	A 7	A 8	A 9
     checkboc_columns = ["Estaba estacionado/parado",
